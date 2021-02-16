750e614a6af06700f48b9ac164d781a6cbafe6db
mahdi hoseyni <mahdihos38@gmail.com>
2021-02-17 02:57:43 +0330

    change app name

--- ticketing_project/send_client/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SendClientConfig(AppConfig):
-    name = 'send_client'
--- ticketing_project/ticketing_project/settings.py
+++ ticketing_project/ticketing_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'send_client.apps.SendClientConfig',
+    'send.apps.SendConfig',
 
 ]
 
